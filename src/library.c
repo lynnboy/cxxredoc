43db2371a19e4dd6da9f7afa0691e9f5654bc9af
Li Yinan <li.yinan.lvu@gmail.com>
2015-04-26 10:25:59 +0800

    [library] en

---
+++ src/library.c
@@ -0,0 +1,15 @@
+﻿[section#library.c
+    [:en] The C standard library
+]
+
+[para]
+[:en]
+The [=Cpp] standard library also makes available the facilities of the C
+standard library, [%library[!C standard]] suitably adjusted to ensure static
+type safety.
+
+[para]
+[:en]
+The descriptions of many library functions rely on the C standard library for
+the signatures and semantics of those functions. In all such cases, any use of
+the [`restrict] qualifier shall be omitted.
